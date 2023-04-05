f0e26884e62b37f742b34b07232255b11e06b0c3
Sylvester254 <slynganga59@gmail.com>
2023-04-06 02:32:17 +0300

    files

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const AuthController = require('../controllers/AuthController');
 const UsersController = require('../controllers/UsersController');
+const FilesController = require('../controllers/FilesController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
@@ -11,5 +13,10 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', authMiddleware, FilesController.getShow);
+router.get('/files', authMiddleware, FilesController.getIndex);
+router.post('/files', authMiddleware, FilesController.postUpload);
+
 
 module.exports = router;
